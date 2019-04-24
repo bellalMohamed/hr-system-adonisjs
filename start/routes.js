b543f81bf7d8ad199bd29d150b6ef41038063f65
Bellal Mohamed <mbellal2000@gmail.com>
2019-04-24 11:49:36 +0200

    JWT TOken

--- start/routes.js
+++ start/routes.js
@@ -1,16 +1,8 @@
 'use strict'
-
-
-/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.group(() => {
     Route.post('employees', 'EmployeeController.all');
-
-}).prefix('api/v1').middleware(['auth']);
-
-Route.group(() => {
-    Route.post('login', 'AuthController.login');
     Route.get('departments', 'DepartmentController.all');
     Route.post('department/create', 'DepartmentController.create');
     Route.post('department/delete', 'DepartmentController.delete');
@@ -19,7 +11,8 @@ Route.group(() => {
     Route.post('employee/create', 'EmployeeController.create');
     Route.post('employee/delete', 'EmployeeController.delete');
     Route.post('employee/update', 'EmployeeController.update');
+}).prefix('api/v1').middleware(['auth']);
 
+Route.group(() => {
+    Route.post('login', 'AuthController.login');
 }).prefix('api/v1').middleware(['guest']);
-
-
